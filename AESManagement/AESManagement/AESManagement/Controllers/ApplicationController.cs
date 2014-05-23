d7e3e0b71613eaf8e8d2350223a9026806a4e78c
Boese <christopher.boese@oit.edu>
2014-05-23 11:39:48 -0700

    Added application view and controller

---
+++ AESManagement/AESManagement/AESManagement/Controllers/ApplicationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AESManagement.AESDataService;
+
+namespace AESManagement.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        //
+        // GET: /Application/
+        public ActionResult Application(int id = 1)
+        {
+            AESManagement.AESDataService.ApplicantApp model = new AESManagement.AESDataService.ApplicantApp();
+            using (DataServiceClient client = new DataServiceClient())
+                model = client.getApplication(id);
+            return View(model);
+        }
+	}
+}
