135668dfb3b6cab089b1cedbac8f43fe0ff8f3c7
Dustin Falgout <dustin.falgout@oit.edu>
2014-05-23 11:12:48 -0700

    added project

---
+++ AESManagement/AESManagement/AESManagement/Models/ApplicantModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AESManagement.Models
+{
+    public class ApplicantModels
+    {
+        public AESDataService.Applicant Applicant { get; set; }
+    }
+}
