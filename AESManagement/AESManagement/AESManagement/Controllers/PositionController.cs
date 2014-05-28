b22201428b501068404934ad5f5a1ea148a742bc
Dustin Falgout <dustin.falgout@oit.edu>
2014-05-27 17:26:59 -0700

    Added crud for Positions

---
+++ AESManagement/AESManagement/AESManagement/Controllers/PositionController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AESManagement.Controllers
+{
+    [Authorize]
+    public class PositionController : Controller
+    {
+        //
+        // GET: /Position/
+        public async Task<ActionResult> Index()
+        {
+            IList<AESDataService.Position> model = new List<AESDataService.Position>();
+            using (AESDataService.DataServiceClient client = new AESDataService.DataServiceClient())
+            {
+                model = await client.getAllPositionsAsync();
+            }
+            return View(model);
+        }
+
+        //
+        // GET: /Position/Create
+        public ActionResult Create()
+        {
+            Models.PositionModel model = new Models.PositionModel();
+            return View(model);
+        }
+
+        //
+        // POST: /Position/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(FormCollection collection)
+        {
+            try
+            {
+                AESDataService.Position position = new AESDataService.Position();
+                using (AESDataService.DataServiceClient client = new AESDataService.DataServiceClient())
+                {
+                    position.title = Request.Form["title"];
+                    position.education = Request.Form["education"];
+                    position.description = Request.Form["description"];
+                    position.requirements = Request.Form["requirements"];
+                    position.pay = Request.Form["pay"];
+                    await client.updatePositionAsync(position);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Position/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            AESManagement.Models.PositionModel model = new Models.PositionModel();
+            AESDataService.Position position = new AESDataService.Position();
+            using (AESDataService.DataServiceClient client = new AESDataService.DataServiceClient())
+            {
+                position = await client.getPositionAsync(id);
+                model.Id = position.positionId;
+                model.Title = position.title;
+                model.Description = position.description;
+                model.Education = position.education;
+                model.Requirements = position.requirements;
+                model.Pay = position.pay;
+                await client.updatePositionAsync(position);
+            }
+            return View(model);
+        }
+
+        //
+        // POST: /Position/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                AESDataService.Position position = new AESDataService.Position();
+                using (AESDataService.DataServiceClient client = new AESDataService.DataServiceClient())
+                {
+                    position.positionId = id;
+                    position.title = Request.Form["title"];
+                    position.education = Request.Form["education"];
+                    position.description = Request.Form["description"];
+                    position.requirements = Request.Form["requirements"];
+                    position.pay = Request.Form["pay"];
+                    await client.updatePositionAsync(position);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AESManagement/AESManagement/AESManagement/Models/PositionModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AESManagement.Models
+{
+    public class PositionModel
+    {
+        [DisplayName("Position #")]
+        public int Id { get; set; }
+
+        [DisplayName("Position Title")]
+        public string Title { get; set; }
+
+        [DisplayName("Requirements")]
+        public string Requirements { get; set; }
+
+        [DisplayName("Education")]
+        public string Education { get; set; }
+
+        [DisplayName("Description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DisplayName("Hourly Pay")]
+        public string Pay { get; set; }
+    }
+}
