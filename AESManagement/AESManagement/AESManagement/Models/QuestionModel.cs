fbeffb1c5f8a9e4f6dcad9e712705bfce845f769
Dustin Falgout <dustin.falgout@oit.edu>
2014-05-29 11:25:07 -0700

    Updated question to radio buttons for setting answer

--- AESManagement/AESManagement/AESManagement/Models/QuestionModel.cs
+++ AESManagement/AESManagement/AESManagement/Models/QuestionModel.cs
@@ -13,12 +13,11 @@ namespace AESManagement.Models
 
         [Required(ErrorMessage = "*")]
         [DisplayName("Question")]
+        [DataType(DataType.MultilineText)]
         public string Question { get; set; }
 
         [Required(ErrorMessage = "*")]
         [DisplayName("Correct Answer")]
-        [MaxLength(1)]
-        [RegularExpression(@"[yn]{1,1}$", ErrorMessage = "Answer must be y or n")]
         public string Answer { get; set; }
     }
 }
